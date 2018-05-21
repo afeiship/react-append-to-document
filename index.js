519dfaea3c48d3263b751c5d6566eba8958e5275
afeiship <1290657123@qq.com>
2018-05-21 17:23:21 +0800

    issue: create element

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var createElement = function (inAttrs, inName) {
+  var element = document.createElement(inName || 'div');
+  for (var attr in inAttrs) {
+    if (inAttrs.hasOwnProperty(attr)) {
+      element[attr] = inTarget[attr];
+    }
+  }
+  return element;
+};
 
-module.exports = function(inComponent,inProps,inTarget){
+module.exports = function (inComponent, inProps, inTarget) {
   var props = inProps || {};
   var isElement = inTarget.nodeType == 1;
   var body = document.body;
-  var element = isElement ? inTarget : document.createElement('div');
-
-  if(!isElement){
-    for (var attr in inTarget) {
-      if (inTarget.hasOwnProperty(attr)) {
-        element[attr]=inTarget[attr];
-      }
-    }
-  }
+  var element = isElement ? inTarget : createElement(inTarget);
 
   body.appendChild(element);
 
   var component = ReactDOM.render(
-    React.createElement(inComponent,inProps)
+    React.createElement(inComponent, inProps)
     , element);
 
   return {
     element: element,
     component: component,
-    destroy: function(){
-      try{
+    createElement: createElement,
+    destroy: function () {
+      try {
         ReactDOM.unmountComponentAtNode(element);
         body.removeChild(element);
-      }catch(_){
+      } catch (_) {
         console.warn(_);
       }
     }
